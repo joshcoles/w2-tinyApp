c7a37f7989081309c037f0b18557c95f9ecbd37f
Josh Coles <josh@joshcoles.com>
2016-11-09 01:21:36 +0000

    Added requesting routing and post request. Assigned long url to new randomly generated short url. Assigned link on 'urls_index.ejs' to point back to 'urls_new.ejs' file.

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 var urlDatabase = {
@@ -26,13 +28,34 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {shortURL: req.params.id,
                       fullURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  // console.log(req.body);
+  //assign long url value to the value of what's put in text field
+  let longURL = req.body.longURL;
+  //assign shorturl to generated random string
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString () {
+  return Math.random().toString(36).substr(2, 6);
+}
+
+
+
