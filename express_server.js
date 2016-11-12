7427a4dec69dcc9900610fb0f5d695b1b08052f1
Josh Coles <josh@joshcoles.com>
2016-11-12 22:57:13 +0000

    Refactored.

--- express_server.js
+++ express_server.js
@@ -4,54 +4,48 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
-const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
+const cookieSession = require("cookie-session");
+//users database is required from separate .json file and is further populated from POST to /register.
+const users = require("./users.json");
+const bcrypt = require("bcrypt");
 
 //================= MIDDLEWARE =========================
+
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(cookieSession({
-  name: 'session',
-  keys: ['joshkey', 'secretkey'],
-
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
-
-
+  name: "session",
+  keys: ["joshkey", "secretkey"],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+app.set("trust proxy", 1);
+app.set("view engine", "ejs");
 
 //For every request, this assigns the value of
-//idOfUserFromCookie to the cookie called "userID".
+//idOfUserFromSession to the session called "userID".
 
-//The current user is equal to the user from the user object that
+//The current user is set to equal the user from the user object that
 //matches IDs with the cookie.
 
-//If there is no current user and the path is not /login or /register
-//then redirect to /login, which now includes a link to /register.
+//If there is no current user and the path is not one of the listed endpoints
+//then redirect to /login
 
 app.use(function(req, res, next) {
-  let idOfUserFromCookie = req.session.userID;
-  req.currentUser = users[idOfUserFromCookie];
-  if (req.currentUser === undefined && (req.path !== "/login" && req.path !== "/register" && req.path !=="/urls/new" && req.path !=="/urls" && !req.path.startsWith("/u/"))) {
+  let idOfUserFromSession = req.session.userID;
+  req.currentUser = users[idOfUserFromSession];
+  if (req.currentUser === undefined && (req.path !== "/login" && req.path !== "/register" && req.path !== "/urls/new" && req.path !== "/urls" && !req.path.startsWith("/u/"))) {
     res.redirect("/login");
-    // res.render("unauthorized");
     return;
-    // res.send('Email already taken');
   } else {
     next();
   }
-})
-
-app.set('trust proxy', 1)
-app.set("view engine", "ejs");
-
-
+});
 
 //================= Data ================================
-
 
 //Re-structured url database. Each URL is assigned to to userID
 //of the user that created it.
 
+//test urls, assigned to pre-existing users imported from users.json. Makes testing faster.
 const urlDatabase = {
   "9sm5xk": {
     "userID": "lkjhgfghjkl",
@@ -64,16 +58,6 @@ const urlDatabase = {
     "longURL": "http://lighthouselabs.ca"
   }
 };
-
-
-
-
-//User database. Empty to begin with except for a pre-programmed
-//users 'a@a.com' and josh@joshcoles.com which is imported from
-//this .json file.
-
-//Populated by filling in and submitting form on register page.
-const users = require('./users.json');
 
 //==================== FUNCTIONS ==========================
 
@@ -89,41 +73,38 @@ function generateRandomUserID () {
 
 //Used to fix URLs that don't include 'www.' or 'http://' in
 //both url creation and editing
-function fixURL(longURL) {
-  if (!(longURL.includes("://"))) {
-    longURL = "http://" + longURL;
+function fixURL(originalURL) {
+  if (!(originalURL.includes("://"))) {
+    originalURL = "http://" + originalURL;
   }
-  return longURL;
+  return originalURL;
 }
 
 //============= Home Page / Login ===================
 
-
-//automatically redirects to registration page
+//automatically redirects to login page
 app.get("/", (req, res) => {
- res.redirect("/login");
+  res.redirect("/login");
 });
 
-//
+//redirects to urls if logged in
+//otherwise renders register
 app.get("/register", (req, res) => {
   if (req.currentUser) {
-    res.redirect("/login");
+    res.redirect("/urls");
   } else {
     let templateVars = {
-    email: req.session.userID
-  };
+      email: req.session.userID
+    };
     res.render("register", templateVars);
   }
-
-})
-
-
+});
 
 //Renders login page, unless user is already signed in. If user is
 //signed in, redirect to /urls.
 app.get("/login", (req, res) => {
   if (req.currentUser) {
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
     res.render("login");
   }
@@ -131,56 +112,27 @@ app.get("/login", (req, res) => {
 
 //When a user submits login information, their email and password are
 //assigned to loginEmail and loginPassword.
-
 app.post("/login", (req, res) => {
   let loginEmail = req.body.email;
   let loginPassword = req.body.password;
 
-  //console.log(loginPassword);
-
-  //This checks loginEmail and loginPassword against our user object
-  //AKA registered users database.
-
-  //If they both match, set cookie as { "userID": 293587hsdgo823h3gg }
-  //and redirect to "/urls". This is the breakpoint where users are
-  //either allowed to access the site or they are not.
-
-
-
-  // 1) figure out which user the request wants to log in as
-  // 2) did that work out?
-  //    2a, yes i) make a call to bcrypt.compare.
-  //            ii) did that work?
-  //                  yes ) log them in
-  //                  no  ) do something else (redir to login page?)
-  //    2b, no  i) do something else (redir to login page?)
-
-  // console.log("users:", users);
-
-  var user = null;
-  for (userId in users) {
-    var user_candidate = users[userId];
-    if (user_candidate.email === loginEmail) {
-      user = user_candidate;
+//decides which user's email will be checked in next section
+  let user = null;
+  for (let userId in users) {
+    let userCandidate = users[userId];
+    if (userCandidate.email === loginEmail) {
+      user = userCandidate;
     }
   }
 
+//user is decided, user's password is checked (with bcrypt.compare) against
+//loginPassword. Error redirects to /login, non-matching passwords renders 401 error,
+//successful match redirects to /urls.
   if (user === null) {
-    // res.redirect("/login");
-    res.status(403).send("Incorrect email or password. Please register or check your password.")
-
+    res.status(403).send("Incorrect email or password. Please register or check your password.");
   } else {
-    // var samePass = bcrypt.compareSync(loginPassword, user.password);
-    // if (samePass){
-    //   yay();
-    // } else {
-    //   boo();
-    // }
-
-
     bcrypt.compare(loginPassword, user.password, function(err, passwordMatches){
       if (err) {
-        // console.log("error in bcrypt?", err);
         res.redirect("/login");
       } else if (!passwordMatches) {
         res.status(401).render("401");
@@ -190,52 +142,15 @@ app.post("/login", (req, res) => {
       }
     });
   }
-
-
-
-  // 1) make a function F to see if this req has a correct password
-  // 2) for all users, see if they match on username (using ===) and on password (using F)
-  // 3) if there was more than one of those, or zero of those, freak out
-  // 4) if there was one of those, log them in
-
-
-  // var arePasswordsTheSame = bcrypt.compare(loginPassword, users[i].password, function (err, passwordMatches) {
-  //   if (passwordMatches) {
-  //     return true;
-  //   } else {
-  //     res.status(403).send("wrong password mannnn");
-  //   }
-  // })
-
-  // for (let i in users) {
-  //   if (true && (loginEmail === users[i].email)) {
-  //     res.cookie("userID", users[i].id);
-  //     res.redirect("/urls");
-  //     return;
-  //   }
-  // }
-
-  //When logging in, this throws an error if the loginEmail or
-  //loginPassword do not match the users object data.
-
-  // for (let i in users) {
-  //   if ((users[i].email !== loginEmail) || true ) {
-  //     res.status(403).send("Incorrect email or password. Please register or check your password.")
-  //     return;
-  //   }
-  // }
-
-});  // app.post("/login", ....)
-
+});
 
 //================ ADD URL ==========================
 
-//If user is logged in, render new URL submission page.
-
+//if user is logged in, renders new URL submission page.
+//otherwise render 401 page.
 app.get("/urls/new", (req, res) => {
-
   if (req.currentUser === undefined) {
-    res.status(401).render("unauthorized");
+    res.status(401).render("401");
   } else {
     res.render("urls_new", {
       email: req.currentUser.email
@@ -245,117 +160,81 @@ app.get("/urls/new", (req, res) => {
 
 //=============== LIST OF URLS =======================
 
-//Renders urls_index (list of urls).
-
+//if not logged in, renders 418 easter egg (You found it!);
 app.get("/urls", (req, res) => {
 
-
   if (req.currentUser === undefined) {
-    res.status(418).render("unauthorized-418");
+    res.status(418).render("418");
   } else {
-  //Exports data
-    //urlDatabase becomes available for display using forEach loop
-    //email becomes available for display in navBar
 
+//loops over urlDatabase and pushes each url that is associate with
+//current user (req.currentUser.id), which is assigned to
+//templateVars and exported to urls_index.
     var currentUsersURLs = [];
-
     for (var url in urlDatabase) {
       if (urlDatabase[url].userID === req.currentUser.id) {
         currentUsersURLs.push(urlDatabase[url]);
       }
     }
-
-
     let templateVars = {
       urls: currentUsersURLs,
       email: req.currentUser.email
     };
-
-    console.log("Cookie created for the username: " + "'" + req.session.userID + "'.");
-    res.render("urls_index", templateVars)
-
-
+    res.render("urls_index", templateVars);
   }
-
 });
 
-
-
-
-
-
+//creates a new tinyURL
+//shortURL is created by generateRandomString(); and longURL is
+//fixed (if necessary) by fixURL();
+//tinyURL is then assigned to urlDatabase, with currentUser's id attached to it
 app.post("/urls", (req, res) => {
-
-    let longURL = req.body.longURL;
-    let shortURL = generateRandomString();
-    // console.log(urlDatabase);
-    // console.log(shortURL);
-    // console.log(urlDatabase[shortURL]);
-    urlDatabase[shortURL] = {
-      shortURL: shortURL,
-      longURL: fixURL(longURL),
-      userID: req.currentUser.id
-    }
-    res.redirect(`/urls/${shortURL}`);
-
-    res.render("unauthorized");
-
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    shortURL: shortURL,
+    longURL: fixURL(longURL),
+    userID: req.currentUser.id
+  };
+  res.redirect(`/urls/${shortURL}`);
 });
-
-
 
 //=================== UNIQUE URL ====================
 
+//displays page specific to a tinyURL with option to update (edit) or delete.
 app.get("/urls/:id", (req, res) => {
-// let idOfUserFromCookie = req.cookies["userID"];
-// let currentUser = users[idOfUserFromCookie];
-
   let templateVars = {
     shortURL: req.params.id,
     fullURL: urlDatabase[req.params.id].longURL,
     username: req.session.userID,
     email: req.currentUser.email
-  }
-console.log(urlDatabase);
-  res.render("urls_show", templateVars)
-
+  };
+  res.render("urls_show", templateVars);
 });
 
+//updates tinyURL
 app.post("/urls/:id", (req, res) => {
-
   let updatedURL = req.body.longURL;
   urlDatabase[req.params.id].longURL = fixURL(updatedURL);
-  console.log(urlDatabase[req.params.id].longURL);
   res.redirect(`/urls/${req.params.id}`);
 });
 
+//redirects to longURL, unless url is invalid in which case renders 404
 app.get("/u/:shortURL", (req, res) => {
   for (var url in urlDatabase) {
-    // console.log(req.params.shortURL);
-    // console.log("url.shorturl", urlDatabase[url].shortURL);
     if (req.params.shortURL === urlDatabase[url].shortURL) {
-      // let longURL = urlDatabase[req.params.shortURL].longURL;
       res.redirect(urlDatabase[req.params.shortURL].longURL);
       return;
     }
-      // console.log(req.params.shortURL, url.shortURL, url[shortURL]);
-
   }
-
-      res.status(404).render("404");
-      return;
-  // if (longURL !== undefined) {
-  //   res.status(404).render("404")
-  // } else {
-  //   let longURL = urlDatabase[req.params.shortURL].longURL;
-  //   res.redirect(longURL);
+  res.status(404).render("404");
+  return;
 });
-
 
 //=============== DELETE =============================
 
+//deletes tinyURL from urlDatabase
 app.post("/urls/:id/delete", (req, res) =>{
-
   let shortURL = req.params.id;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
@@ -363,47 +242,44 @@ app.post("/urls/:id/delete", (req, res) =>{
 
 //==================== JSON ============================
 
+//provides .json version of urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 //==================== LOGOUT ==========================
 
+//logs out, redirects to /login and deletes cookie/session
 app.post("/logout", (req, res) => {
-
   req.session.userID = null;
-  // res.clearCookie("userID");
   console.log("Cookie deleted.");
-
   res.redirect("/login");
 });
 
 //================= USER AUTHENTICATION ==================
 
-
+//if either email or password field is empty, returns 400 and renders "400-empty-field"
+//if email already exists in user database, returns 400 and renders "400-duplicate"
 app.post("/register", (req, res) => {
 
-  // debugger;
- if ((req.body.email === "") || (req.body.password === "")) {
-  res.status(400).render("400-empty-field");
-
- } else {
-
-  for (var i in users) {
-    if (req.body.email === users[i].email) {
-      res.status(400).render("400-duplicate");
+  if ((req.body.email === "") || (req.body.password === "")) {
+    res.status(400).render("400-empty-field");
+  } else {
+    for (let i in users) {
+      if (req.body.email === users[i].email) {
+        res.status(400).render("400-duplicate");
+      }
     }
+//else, assigns user to user database including randomly-generated ID and
+//hashed password
+    let userID = generateRandomUserID();
+    users[userID] = {};
+    users[userID].id = userID;
+    users[userID].email = req.body.email;
+    users[userID].password = bcrypt.hashSync(req.body.password, 10);
+    res.redirect("/login");
   }
-  let userID = generateRandomUserID();
-  users[userID] = {};
-  users[userID].id = userID;
-  users[userID].email = req.body.email;
-  users[userID].password = bcrypt.hashSync(req.body.password, 10);
-  console.log(users);
-
-  res.redirect("/login");
-  }
-})
+});
 
 //================== LISTEN ==============================
 
