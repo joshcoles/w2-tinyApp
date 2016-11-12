0e398dff3a3aa90efecb6d7a12928363830ee187
Josh Coles <josh@joshcoles.com>
2016-11-12 00:02:19 +0000

    Refactored lots of code. Restructured url data object to include id of user that created link, adjusted templates and get/post requests accordingly. Bulked up middleware to handle all redirects to /login and check for cookies.

--- express_server.js
+++ express_server.js
@@ -11,10 +11,24 @@ const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+
+//For every request, this assigns the value of
+//idOfUserFromCookie to the cookie called "userID".
+
+//The current user is equal to the user from the user object that
+//matches IDs with the cookie.
+
+//If there is no current user and the path is not /login or /register
+//then redirect to /login, which now includes a link to /register.
+
 app.use(function(req, res, next) {
-  let idOfUserFromCookie = req.cookies["User-ID"];
+  let idOfUserFromCookie = req.cookies["userID"];
   req.currentUser = users[idOfUserFromCookie];
-  next();
+  if (req.currentUser === undefined && (req.path !== "/login" && req.path !== "/register" && !req.path.startsWith("/u/"))) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
 })
 
 app.set('trust proxy', 1)
@@ -22,25 +36,49 @@ app.set("view engine", "ejs");
 
 
 
-//================= Data ==========================
+//================= Data ================================
+
+
+//Re-structured url database. Each URL is assigned to to userID
+//of the user that created it.
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xk": {
+    "userID": "lkjhgfghjkl",
+    "shortURL": "9sm5xk",
+    "longURL": "http://www.google.com"
+  },
+  "b2xVn2": {
+    "userID": "ggggttttttt",
+    "shortURL": "b2xVn2",
+    "longURL": "http://lighthouselabs.ca"
+  }
 };
 
+
+
+
+//User database. Empty to begin with except for a pre-programmed
+//users 'a@a.com' and josh@joshcoles.com which is imported from
+//this .json file.
+
+//Populated by filling in and submitting form on register page.
 const users = require('./users.json')
 
 //==================== FUNCTIONS ==========================
 
+//Used for creating random string for shortURL.
 function generateRandomString () {
   return Math.random().toString(36).substr(2, 6);
 }
 
+//Used for generating unique ID for each user in Users object.
 function generateRandomUserID () {
   return Math.random().toString(36).substr(2, 20);
 }
 
+//Used to fix URLs that don't include 'www.' or 'http://' in
+//both url creation and editing
 function fixURL(longURL) {
   if (!(longURL.includes("://"))) {
     longURL = "http://" + longURL;
@@ -51,129 +89,131 @@ function fixURL(longURL) {
 //============= Home Page / Login ===================
 
 
-
+//automatically redirects to registration page
 app.get("/", (req, res) => {
- res.redirect("/register");
+ res.redirect("/login");
 });
 
-
+//
 app.get("/register", (req, res) => {
   let templateVars = {
-    email: req.cookies["User-ID"]
+    email: req.cookies["userID"]
   };
     res.render("register", templateVars);
 })
 
 
-
+//Renders login page, unless user is already signed in. If user is
+//signed in, redirect to /urls.
 app.get("/login", (req, res) => {
-
-
-  res.render("login");
-
+  if (req.currentUser) {
+    res.redirect("/urls")
+  } else {
+    res.render("login");
+  }
 });
 
+//When a user submits login information, their email and password are
+//assigned to loginEmail and loginPassword.
 
 app.post("/login", (req, res) => {
   let loginEmail = req.body.email;
   let loginPassword = req.body.password;
 
-  console.log("Login email: " + loginEmail);
-  console.log("Login password: " + loginPassword);
+//This checks loginEmail and loginPassword against our user object
+//AKA registered users database.
 
+//If they both match, set cookie as { "userID": 293587hsdgo823h3gg }
+//and redirect to "/urls". This is the breakpoint where users are
+//either allowed to access the site or they are not.
 
   for (let i in users) {
     if ((loginPassword === users[i].password) && (loginEmail === users[i].email)) {
-      res.cookie("User-ID", users[i].id)
+      res.cookie("userID", users[i].id);
       res.redirect("/urls");
-      // return;
+      return;
     }
   }
+
+//When logging in, this throws an error if the loginEmail or
+//loginPassword do not match the users object data.
 
   for (let i in users) {
     if ((users[i].email !== loginEmail) || (users[i].password !== loginPassword) ) {
       res.status(403).send("Incorrect email or password. Please register or check your password.")
+      return;
     }
   }
 
-  // res.redirect("/login")
 });
+
 
 //================ ADD URL ==========================
 
+//If user is logged in, render new URL submission page.
 
 app.get("/urls/new", (req, res) => {
-  /*if (!req.cookies["User-ID"]) {
-    res.redirect("/login");
-  }
-  let currentUser = users[req.cookies["User-ID"]].
   res.render("urls_new", {
-    email: currentUser.username;
-  });*/
-
-  // let idOfUserFromCookie = req.cookies["User-ID"];
-  // let currentUser = users[idOfUserFromCookie];
-
-    if (req.currentUser) {
-      res.render("urls_new", {
-        email: req.currentUser.email
-      });
-    } else {
-      res.redirect("/login");
-      return;
-    }
+    email: req.currentUser.email
+  });
 });
 
 
 //=============== LIST OF URLS =======================
 
+//Renders urls_index (list of urls).
+
 app.get("/urls", (req, res) => {
 
-  let idOfUserFromCookie = req.cookies["User-ID"];
-  let currentUser = users[idOfUserFromCookie];
+//Exports data
+  //urlDatabase becomes available for display using forEach loop
+  //email becomes available for display in navBar
+
+
+  //fill array with urls (objects) that belong to
+  //req.currentUser
+  //check that userID is equal to req.currentUser.id
+  //push to currentUsersURLs
+  //pass that into templatevars -> urls
+  var currentUsersURLs = [];
+
+  for (var url in urlDatabase) {
+    if (urlDatabase[url].userID === req.currentUser.id) {
+      currentUsersURLs.push(urlDatabase[url]);
+    }
+  }
+
+  // console.log("User id: " + req.currentUser.id);
+  // console.log("new array" + currentUsersURLs);
+
 
   let templateVars = {
-    urls: urlDatabase,
-    email: currentUser.email
+    urls: currentUsersURLs,
+    email: req.currentUser.email
   };
 
-  if (currentUser) {
-    console.log("Cookie created for the username: " + "'" + req.cookies["User-ID"] + "'.");
-    res.render("urls_index", templateVars)
-  } else {
-    res.redirect("/login");
-  }
+
+//
+  console.log("Cookie created for the username: " + "'" + req.cookies["userID"] + "'.");
+  res.render("urls_index", templateVars)
+
 });
 
 
 app.post("/urls", (req, res) => {
 
-  let idOfUserFromCookie = req.cookies["User-ID"];
-  let currentUser = users[idOfUserFromCookie];
-
-  if (currentUser) {
     let longURL = req.body.longURL;
     let shortURL = generateRandomString();
-    urlDatabase[shortURL] = fixURL(longURL);
+    // console.log(urlDatabase);
+    // console.log(shortURL);
+    // console.log(urlDatabase[shortURL]);
+    urlDatabase[shortURL] = {
+      shortURL: shortURL,
+      longURL: fixURL(longURL),
+      userID: req.currentUser.id
+    }
     res.redirect(`/urls/${shortURL}`);
-  } else {
-    res.redirect("/login");
-  }
 
-  //Is this the same as above?
-
-  // if (!req.cookies["User-ID"]) {
-  //   res.redirect("/login");
-  // }
-  // //assign long url value to the value of what's put in text field
-  // let longURL = req.body.longURL;
-  // //assign shorturl to generated random string
-  // let shortURL = generateRandomString();
-  // //assign short and long url as key value pairs
-  // urlDatabase[shortURL] = fixURL(longURL);
-  // //after assigning, redirect browser to url pair list
-
-  // res.redirect(`/urls/${shortURL}`);
 });
 
 
@@ -181,53 +221,31 @@ app.post("/urls", (req, res) => {
 //=================== UNIQUE URL ====================
 
 app.get("/urls/:id", (req, res) => {
-let idOfUserFromCookie = req.cookies["User-ID"];
-let currentUser = users[idOfUserFromCookie];
+// let idOfUserFromCookie = req.cookies["userID"];
+// let currentUser = users[idOfUserFromCookie];
 
-let templateVars = {
+  let templateVars = {
     shortURL: req.params.id,
-    fullURL: urlDatabase[req.params.id],
-    username: req.cookies["User-ID"],
-    email: currentUser.email
-  };
-
-
-  if (currentUser) {
-    let templatevars = {
-      shortURL: req.params.id,
-      fullURL: urlDatabase[req.params.id],
-      username: req.cookies["User-ID"]
-    }
-    res.render("urls_show", templateVars)
-  } else {
-    res.redirect("/login");
+    fullURL: urlDatabase[req.params.id].longURL,
+    username: req.cookies["userID"],
+    email: req.currentUser.email
   }
+console.log(urlDatabase);
+  res.render("urls_show", templateVars)
 
-
-  // if (!req.cookies["User-ID"]) {
-  //   res.redirect("/login");
-  // }
-  // let templateVars = {
-  //   shortURL: req.params.id,
-  //   fullURL: urlDatabase[req.params.id],
-  //   username: req.cookies["User-ID"]
-  // };
-  // res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:id", (req, res) => {
-  if (!req.cookies["User-ID"]) {
-    res.redirect("/login");
-  }
-  let updatedURL = req.body.updatedURL;
-  urlDatabase[req.params.id] = fixURL(updatedURL);
-  console.log(urlDatabase[req.params.id]);
+
+  let updatedURL = req.body.longURL;
+  urlDatabase[req.params.id].longURL = fixURL(updatedURL);
+  console.log(urlDatabase[req.params.id].longURL);
   res.redirect(`/urls/${req.params.id}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
 
-  let longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
@@ -235,20 +253,11 @@ app.get("/u/:shortURL", (req, res) => {
 //=============== DELETE =============================
 
 app.post("/urls/:id/delete", (req, res) =>{
-  if (!req.cookies["User-ID"]) {
-    res.redirect("/login");
-  }
+
   let shortURL = req.params.id;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
-
-app.get("/u/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
 
 //==================== JSON ============================
 
@@ -260,11 +269,8 @@ app.get("/urls.json", (req, res) => {
 //==================== LOGOUT ==========================
 
 app.post("/logout", (req, res) => {
-  // Object.keys(req.cookies).forEach(function(property) {
-  //   res.clearCookie(property);
-  // });
 
-  res.clearCookie("User-ID");
+  res.clearCookie("userID");
   console.log("Cookie deleted.");
 
   res.redirect("/login");
@@ -282,6 +288,7 @@ app.post("/register", (req, res) => {
     if (req.body.email === users[i].email) {
       res.status(400);
       res.send('Email already taken');
+      return;
     }
   }
 
@@ -293,9 +300,6 @@ app.post("/register", (req, res) => {
   console.log(users);
 
   res.redirect("/login");
-
-  // console.log("users.userID.id: " + users.userID.id);
-  // console.log("users.userID.email: " + users.userID.email);
 })
 
 
